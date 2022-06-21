39ef976c8c0cc801c1b8ef9a98a4b5e543f37315
ariielm <arielhenrique3@gmail.com>
2022-06-21 18:11:07 -0300

    Application with properties for K8s probe

---
+++ src/main/java/com/ariielm/springactuator4k8sdemo/SpringActuator4K8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ariielm.springactuator4k8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuator4K8sDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuator4K8sDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ariielm/springactuator4k8sdemo/SpringActuator4K8sDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariielm.springactuator4k8sdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuator4K8sDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
